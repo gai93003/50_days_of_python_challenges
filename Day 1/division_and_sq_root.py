22de109e03e6f7270c0adade082b516f09acdce2
Gabriel <deng93003@gmail.com>
2026-05-03 11:42:00 +0100

    Added readme and Day 1

---
+++ Day 1/division_and_sq_root.py
@@ -0,0 +1,14 @@
+# Write a function called "divide_or_square" that takes one argument(a number), and returns the square root of the number if it is divisible by 5, returns its remainder if it is not divisible by 5. For example, if you pass 10 as an argument, then you function should return 3.16 as the square root.
+
+def divide_or_square(num):
+    reminder = num % 5
+    sqrt = num ** 0.5
+
+    if reminder == 0:
+        return round(sqrt, 2)
+    else:
+        return reminder
+    
+    
+print(divide_or_square(11))
+print(divide_or_square(10))
