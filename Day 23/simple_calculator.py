78876f089acd37176313d02e51235a31a19b65b9
Gabriel <deng93003@gmail.com>
2026-06-12 14:24:45 +0100

    file for simple calculator

---
+++ Day 23/simple_calculator.py
@@ -0,0 +1,7 @@
+######### Day 23: Simple calculator ###########
+
+# Create a simple calculator. The calculator should be able to perform basic math operations, add, subtract, divide and multiple. The calculator should take input from users.
+# The calculator should be able to handle `ZeroDivisionError`, `NameError` and `ValueError`.
+
+def calculator():
+    
