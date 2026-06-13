d3b89957b6ad4cc67a3338a88f29f2944ca141af
Gabriel <deng93003@gmail.com>
2026-06-13 20:20:45 +0100

    Implementation of calculator

--- Day 23/simple_calculator.py
+++ Day 23/simple_calculator.py
@@ -1,7 +1,27 @@
-######### Day 23: Simple calculator ###########
-
-# Create a simple calculator. The calculator should be able to perform basic math operations, add, subtract, divide and multiple. The calculator should take input from users.
-# The calculator should be able to handle `ZeroDivisionError`, `NameError` and `ValueError`.
+import operator
 
 def calculator():
-    
+    try:
+        num1 = int(input("Enter number: "))
+        opt = input("Pick operator (+ - / *) : ")
+        num2 = int(input("Enter another number: "))
+
+        if opt not in ['+', '-', '*', '/'] or len(opt) > 1:
+            print("Please enter a valid operator")
+    except ValueError:
+        print("Please enter a valid number")
+    except ZeroDivisionError:
+        print("You cannot divide a number by zero, Try again")
+    else:
+        if opt == "+":
+            return f"Ans is: {operator.add(num1, num2)}"
+        elif opt == "-":
+            return f"Ans is: {operator.sub(num1, num2)}"
+        elif opt == "*":
+            return f"Ans is: {operator.mul(num1, num2)}"
+        elif opt == "/":
+            return f"Ans is: {operator.truediv(num1, num2)}"
+        
+    return "Try again"
+
+print(calculator())
