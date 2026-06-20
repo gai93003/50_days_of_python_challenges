d8491f534d77274bbe9cb4216bf71d392ebf8b1d
Gabriel <deng93003@gmail.com>
2026-06-20 20:42:06 +0100

    nested lists

---
+++ Day 24/nested_lists.py
@@ -0,0 +1,4 @@
+############# Extra Challenge: Create a Nested List ##############
+
+# Write a function called "nested_lists" that takes any number of lists and creates a 2-dimensional nested list of lists. For example, if you pass the following lists as arguments: [1, 2, 3, 5], [1, 2, 3, 4], [1, 3, 4, 5]
+# Your code should return: [[1, 2, 3, 5], [1, 2, 3, 4], [1, 3, 4, 5]]
