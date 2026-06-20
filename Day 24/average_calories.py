ce1f37c8be0ef849a4e3dee6d0aae92cd916d532
Gabriel <deng93003@gmail.com>
2026-06-20 20:42:25 +0100

    average calories

---
+++ Day 24/average_calories.py
@@ -0,0 +1,3 @@
+############ Day 24: Average Calories ############
+
+# Create a function called "average_calories" that calculates the average calories intake of a user. The function should aks the user to input their calories intake for any number of days and once they hit "done" it should calculate and return the average intake.
